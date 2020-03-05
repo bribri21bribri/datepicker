0a9dd3edacac3ad5504edecbb3624d07ddfdc390
brian.chao <brian.chao@fucotech.com.tw>
2020-03-05 10:06:46 +0800

    max&min date setting

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -13,6 +13,8 @@ export class DatepickerComponent implements OnInit {
   maxDate: moment.Moment;
   localeString = 'zh-tw';
   navDate: any;
+  weekDaysHeaderArr: Array<string> = [];
+  gridArr: Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
@@ -30,28 +32,84 @@ export class DatepickerComponent implements OnInit {
         throw Error('maxDate can not be less then current time');
       }
     }
+    this.makeHeader();
+    this.makeGrid();
   }
 
   changeNavMonth(num: number) {
     if (this.canChangeNavMonth(num)) {
       this.navDate.add(num, 'month');
     }
+    this.makeGrid();
   }
 
   canChangeNavMonth(num: number){
     const clonedDate = moment(this.navDate);
+    const minDateStart = moment(this.minDate).startOf('month');
+    const maxDateStart = moment(this.maxDate).startOf('month');
+    const maxDateEnd = moment(this.maxDate).endOf('month');
     clonedDate.add(num, 'month');
     if (this.minDate && !this.maxDate) {
-      return clonedDate.endOf('month').isSameOrAfter(this.minDate.startOf('month'));
+      return clonedDate.endOf('month').isSameOrAfter(minDateStart);
     } else if (this.maxDate && !this.minDate) {
-      return clonedDate.startOf('month').isSameOrBefore(this.maxDate.endOf('month'));
+      return clonedDate.startOf('month').isSameOrBefore(maxDateEnd);
     } else if (this.minDate && this.maxDate) {
-      return clonedDate.startOf('month').isBetween(this.minDate.startOf('month'), this.maxDate.endOf('month'))
-      || clonedDate.startOf('month').isSame(this.minDate.startOf('month'))
-      || clonedDate.startOf('month').isSame(this.maxDate.startOf('month'));
+      return clonedDate.startOf('month').isBetween(minDateStart, maxDateEnd)
+      || clonedDate.startOf('month').isSame(minDateStart)
+      || clonedDate.startOf('month').isSame(maxDateStart);
     } else {
       return true;
     }
+  }
+
+  makeHeader(){
+    const weekDaysArr: Array<number> = [0, 1, 2, 3, 4, 5, 6];
+    weekDaysArr.forEach(day => this.weekDaysHeaderArr.push(moment().weekday(day).format('ddd')));
+  }
+
+  makeGrid() {
+    this.gridArr = [];
+
+    const firstDayDate = moment(this.navDate).startOf('month');
+    const initialEmptyCells = firstDayDate.weekday();
+    const lastDayDate = moment(this.navDate).endOf('month');
+    const lastEmptyCells = 6 - lastDayDate.weekday();
+    const daysInMonth = this.navDate.daysInMonth();
+    const arrayLength = initialEmptyCells + lastEmptyCells + daysInMonth;
+
+    for (let i = 0; i < arrayLength; i++) {
+      const obj: any = {};
+      if (i < initialEmptyCells || i > initialEmptyCells + daysInMonth - 1) {
+        obj.value = 0;
+        obj.available = false;
+      } else {
+        obj.value = i - initialEmptyCells + 1;
+        obj.available = this.isAvailable(i - initialEmptyCells + 1);
+      }
+      this.gridArr.push(obj);
+    }
+    // console.log(this.gridArr);
+  }
+
+  isAvailable(num: number): boolean {
+    const dateToCheck = this.dateFromNum(num, this.navDate);
+    console.log('dateToCheck', dateToCheck);
+    console.log('minDate', this.minDate);
+    console.log('isBefore', dateToCheck.isSameOrBefore(this.minDate));
+    console.log('maxDate', this.maxDate);
+    console.log('isAfter', dateToCheck.isSameOrAfter(this.maxDate));
+    if (dateToCheck.isSameOrBefore(this.minDate)) {
+        return false;
+    } else if (dateToCheck.isSameOrAfter(this.maxDate)) {
+      return false;
+    } else {
+        return true;
+    }
+  }
+
+  dateFromNum(num: number, referenceDate: any): any {
+    const returnDate = moment(referenceDate);
+    return returnDate.date(num).startOf('day');
   }
 
 }
